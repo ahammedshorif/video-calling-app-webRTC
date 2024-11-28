ac4b4ee28d0402251e0646591c83173fa66029aa
ahammedshorf <ahammedshorif20@gmail.com>
2024-11-29 01:20:26 +0600

    P2P connection done

--- server.js
+++ server.js
@@ -1,36 +1,66 @@
 import express from "express";
-import {createServer} from "http";
-import {Server} from "socket.io"
-import { fileURLToPath} from "url";
-import {dirname,join} from "path";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 const PORT = process.env.PORT ?? 3000;
-//
+const allusers = {};
+
+// /your/system/path
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const allUsers= {};
 
-//exposing the public folder to outside world
-app.use(express.static("public"))
+// exposing public directory to outside world
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/app/index.html');
-}) 
+// handle incoming http request
+app.get("/", (req, res) => {
+    console.log("GET Request /");
+    res.sendFile(join(__dirname + "/app/index.html"));
+});
 
-//handle socket connection
-io.on("connection",(socket)=>{
-    console.log(`someone connected to socket id is ${socket.id}`);
-    socket.on("join-user", (username)=>{
-       console.log(`${username} joined socket connection`);
-       allUsers[username] = {username, id: socket.id};
-       //inform the all users to someones join the socket
+// handle socket connections
+io.on("connection", (socket) => {
+    console.log(`Someone connected to socket server and socket id is ${socket.id}`);
+    socket.on("join-user", username => {
+        console.log(`${username} joined socket connection`);
+        allusers[username] = { username, id: socket.id };
+        // inform everyone that someone joined
+        io.emit("joined", allusers);
+    });
 
-       io.emit("joined",allUsers);
+    socket.on("offer", ({from, to, offer}) => {
+        console.log({from , to, offer });
+        io.to(allusers[to].id).emit("offer", {from, to, offer});
+    });
+
+    socket.on("answer", ({from, to, answer}) => {
+       io.to(allusers[from].id).emit("answer", {from, to, answer});
+    });
+
+    socket.on("end-call", ({from, to}) => {
+        io.to(allusers[to].id).emit("end-call", {from, to});
+    });
+
+    socket.on("call-ended", caller => {
+        const [from, to] = caller;
+        io.to(allusers[from].id).emit("call-ended", caller);
+        io.to(allusers[to].id).emit("call-ended", caller);
     })
-} )
-server.listen(PORT, ()=>{
-    console.log("listening on port 3000");
+
+    socket.on("icecandidate", candidate => {
+        console.log({ candidate });
+        //broadcast to other peers
+        socket.broadcast.emit("icecandidate", candidate);
+    }); 
+})
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port 3000`);
 });
